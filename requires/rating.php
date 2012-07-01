4a0ed262a417ec518644f74c903314dd45830bad
Separator <is5201@mail.ru>
2012-07-02 01:58:19 +0600

    - Почти дописан первый объект для расчёта рейтинга.

--- requires/rating.php
+++ requires/rating.php
@@ -1,11 +1,11 @@
 <?php
-	/*
+	/**
 	 * Вспомогательный класс.
 	 * Обеспечивает потомкам возможность логировать сообщения об ошибках.
 	 */
 	class RWGObject {
 		protected $errors=array();
-		/*
+		/**
 		 * Логирование ошибок, возникающих в объекте
 		 * @param $message Сообщение об ошибке
 		 * @return Boolean false
@@ -14,16 +14,16 @@
 			$this->errors[] = $message;
 			return false;
 		}
-		/*
+		/**
 		 * Получение списка ошибок
 		 * @return {Array} Список ошибок
 		 */
-		protected function get_errors() {
+		public function get_errors() {
 			return $this->errors;
 		}
 	}
 
-	class RWGDBaseWork extends RWGObject {
+	final class RWGDBaseWork extends RWGObject {
 		protected $host;
 		protected $base;
 		protected $user;
@@ -41,10 +41,16 @@
 									inner join stat_players as SP on SG.IDPlayer=SP.IDPlayer 
 									inner join stat_maps as SM    on SG.IDMap=SM.IDMap 
 									inner join stat_mods as SMD   on SG.IDMod=SMD.IDMod 
-									where SG.IDGame={IDGame}"
+									where SG.IDGame={IDGame};",
+			"reset_rating_data" => "delete from stat_ratings_data where IDRating={IDRating};",
+			"get_games_list"    => "select IDGame from stat_games order by Date asc;",
+			"get_players_stats" => "select * from stat_teams as SG 
+									inner join stat_player_stats as SPS on SG.IDTeam=SPS.IDTeam 
+									inner join stat_players as SP on SPS.IDPlayer=SP.IDPlayer 
+									where SG.IDGame={IDGame} order by SG.Number asc"
 		);
 		
-		/*
+		/**
 		 * Подключение к БД
 		 * @return Integer Идентификатор соединения
 		 */
@@ -58,7 +64,7 @@
 				return $this->connection;
 			} catch (Exception $e) {return $this->log_error($e->getMessage());}
 		}
-		/*
+		/**
 		 * Конструктор объекта
 		 * @param $host Хост, на котором крутится БД
 		 * @param $base Имя БД
@@ -76,7 +82,7 @@
 				return true;
 			} catch (Exception $e) {return $this->log_error($e->getMessage());}
 		}
-		/*
+		/**
 		 * Сформировать строку запроса к БД
 		 * @param $request Шаблон запроса
 		 * @param $options Массив опций, используемый при формировании строки запроса
@@ -90,7 +96,7 @@
 				return '';
 			}
 		}
-		/*
+		/**
 		 * Получить двумерный массив из результата запроса к БД
 		 * @param $result Шаблон запроса
 		 * @return Array Результат запроса в виде двумерного массива
@@ -104,7 +110,7 @@
 				return array();
 			}
 		 }
-		/*
+		/**
 		 * Осуществить простой запрос к БД
 		 * @param $query Строка запроса
 		 * @return Array Список с результатами запроса к БД
@@ -119,7 +125,7 @@
 					return $this->result_to_arr($result);
 			} catch (Exception $e) {return $this->log_error($e->getMessage());}
 		}
-		/*
+		/**
 		 * Осуществить заданный запрос к БД
 		 * @param $name Имя запроса
 		 * @param $options Массив опций, используемый при формировании строки запроса
@@ -135,7 +141,7 @@
 		}
 	}
 	
-	/*
+	/**
 	 * Базовый объект работы с рейтингом
 	 */
 	abstract class RWGBaseRating extends RWGObject {
@@ -143,7 +149,7 @@
 		protected $id;
 		protected $options;
 		
-		/*
+		/**
 		 * Конструктор объекта
 		 * @param {Array} $base Настройки для подключения к БД
 		 * @param {Number} $id Идентификатор рейтинга
@@ -151,14 +157,13 @@
 		 */
 		public function __construct(RWGDBaseWork $base, $id) {
 		 	try {
-				$this->$base = $base;
-				$this->id    = $id;
+				$this->base = $base;
+				$this->id   = $id;
 				$this->get_options();
 				return true;
 			} catch (Exception $e) {return $this->log_error($e->getMessage());}
 		}
-		
-		/*
+		/**
 		 * Получение свойств рейтинга
 		 * @return {Boolean} В случае возникновения ошибок возвращается false, иначе true
 		 */
@@ -167,15 +172,21 @@
 				$data = $this->base->request('get_rating_options', array('{IDRating}'=>$this->id));
 				if (!$data)
 					throw new Exception('Не удалось получить настройки рейтинга');
-				$this->options = $data;
-				return $data;
+				// переписываем массив для пущего удобствия:
+				for ($i=0; $i < count($data); $i++)
+					$this->options[$data[$i]['Key']] = $data[$i]['Value'];
+				return $this->options;
 			} catch (Exception $e) {
 				$this->log_error($e->getMessage());
 				$this->options = array();
 				return false;
 			}
 		}
-		
+		/**
+		 * Получение данных по игре
+		 * @param {Number} $gameid id игры
+		 * @return {Boolean} В случае возникновения ошибок возвращается false, иначе true
+		 */
 		protected function get_game_data($gameid) {
 			try {
 				$data = $this->base->request('get_game_data', array('{IDGame}'=>$gameid));
@@ -184,13 +195,244 @@
 				return $data;
 			} catch (Exception $e) {return $this->log_error($e->getMessage());}
 		}
-		
+		/**
+		 * Получить список всех сыгранных игр
+		 * @return {Boolean} В случае возникновения ошибок возвращается false, иначе true
+		 */
+		protected function get_games_list() {
+			try {
+				// пытаемся получить список:
+				$data = $this->base->request('get_games_list');
+				if (!$data)
+					throw new Exception('Не удалось получить список игр');
+				return true;
+			} catch (Exception $e) {return $this->log_error($e->getMessage());}
+		}
+		/**
+		 * Получение данных рейтинга
+		 * @return {Boolean} В случае возникновения ошибок возвращается false, иначе true
+		 */
 		protected function get_rating_data() {
 			try {
-				$data = $this->base->request('get_game_data', array('{IDGame}'=>$gameid));
+				$data = $this->base->request('get_rating_data', array('{IDRating}'=>$this->id));
 				if (!$data)
-					throw new Exception('Не удалось получить данные по игре');
+					throw new Exception('Не удалось получить данные по рейтингу');
 				return $data;
+			} catch (Exception $e) {return $this->log_error($e->getMessage());}
+		}
+		/**
+		 * Инициализация рейтинга
+		 * @return {Boolean} В случае возникновения ошибок возвращается false, иначе true
+		 */
+		protected function reset_rating() {
+			try {
+				// удаляем все данные по текущему рейтингу:
+				$data = $this->base->request('reset_rating_data', array('{IDRating}'=>$this->id));
+				if (!$data)
+					throw new Exception('Не удалось сбросить данные по рейтингу');
+				return true;
+			} catch (Exception $e) {return $this->log_error($e->getMessage());}
+		}
+		/**
+		 * Преобразовать стату
+		 * @param {Array} $stats Необработанная стата
+		 * @return {Boolean} В случае возникновения ошибок возвращается false, иначе true
+		 */
+		protected function convert_stats($stats) {
+			try {
+				$result = array();
+				for ($i=0; $i < count($stats); $i++) {
+					$index = $stats[$i]['Number'];
+					if (!$result[$index]) {
+						$result[$index] = array(
+							'Players'    => array(),
+							'IDTeam'     => $stats[$i]['IDTeam'],
+							'Win'        => $stats[$i]['Win'],
+							'IDGame'     => $stats[$i]['IDGame'],
+							'Number'     => $index,
+							'ReplayFile' => $stats[$i]['ReplayFile']
+						);
+					}
+					// Является ли игрок наблюдателем:
+					// по идее нужно перенести проверку в заливку игры,
+					// но неохото туда соваться :(
+					$points =	$stats[$i]['InfantryKills'] + $stats[$i]['InfantryLoses'] + $stats[$i]['TanksKills'] +
+								$stats[$i]['TanksLoses'] + $stats[$i]['TrucksKills'] + $stats[$i]['TrucksLoses'] + 
+								$stats[$i]['AircraftKills'] + $stats[$i]['AircraftLoses'] + $stats[$i]['AntiAircraftKills'] +
+								$stats[$i]['AntiAircraftLoses'] + $stats[$i]['ArtilleryKills'] + $stats[$i]['ArtilleryLoses'] +
+								$stats[$i]['TrainsShipsKills'] + $stats[$i]['TrainsShipsLoses'] +
+								$stats[$i]['UnknownKills'] + $stats[$i]['UnknownLoses'];
+					if ($points == 0)	$points = true;
+					else				$points = false;
+					// заполняем данные по игроку:
+					$result[$index]['Players'][] = array(
+						'IDPlayer'          => $stats[$i]['IDPlayer'],
+						'Name'              => $stats[$i]['Name'],
+						'InfantryKills'     => $stats[$i]['InfantryKills'],
+						'InfantryLoses'     => $stats[$i]['InfantryLoses'],
+						'TanksKills'        => $stats[$i]['TanksKills'],
+						'TanksLoses'        => $stats[$i]['TanksLoses'],
+						'TrucksKills'       => $stats[$i]['TrucksKills'],
+						'TrucksLoses'       => $stats[$i]['TrucksLoses'],
+						'AircraftKills'     => $stats[$i]['AircraftKills'],
+						'AircraftLoses'     => $stats[$i]['AircraftLoses'],
+						'AntiAircraftKills' => $stats[$i]['AntiAircraftKills'],
+						'AntiAircraftLoses' => $stats[$i]['AntiAircraftLoses'],
+						'ArtilleryKills'    => $stats[$i]['ArtilleryKills'],
+						'ArtilleryLoses'    => $stats[$i]['ArtilleryLoses'],
+						'TrainsShipsKills'  => $stats[$i]['TrainsShipsKills'],
+						'TrainsShipsLoses'  => $stats[$i]['TrainsShipsLoses'],
+						'UnknownKills'      => $stats[$i]['UnknownKills'],
+						'UnknownLoses'      => $stats[$i]['UnknownLoses'],
+						//'Watcher'           => $stats[$i]['Wathcer']
+						'Watcher'           => $points
+					);
+				}
+				return $result;
+			} catch (Exception $e) {return $this->log_error($e->getMessage());}
+		}
+		/**
+		 * Получить стату игроков для заданной игры
+		 * @param {Number} $gameid id игры
+		 * @return {Boolean} В случае возникновения ошибок возвращается false, иначе true
+		 */
+		protected function get_players_stats($gameid) {
+			try {
+				// пытаемся получить стату:
+				$data = $this->base->request('get_players_stats', array('{IDGame}'=>$gameid));
+				if (!$data)
+					throw new Exception('Не удалось получить стату заданной игры');
+				return $this->convert_stats($data);
+			} catch (Exception $e) {return $this->log_error($e->getMessage());}
+		}
+		/**
+		 * Получить рейтинг игрока
+		 * @param {Number} $playerid id игрока
+		 * @return {Array} Массив вида array('Games'=>'кол-во сыгранных игр', 'Rating'=>'текущий рейтинг')
+		 */
+		abstract public function get_player_rating($playerid);
+		/**
+		 * Учёт игры в текущем рейтинге
+		 * @param {Number} $gameid id игры
+		 * @return {Boolean} Была ли учтена игра
+		 */
+		abstract public function calculate_game($gameid);
+		/**
+		 * Учёт всех сыгранных игр
+		 * @return {Boolean} Были ли учтены все игры
+		 */
+		public function calculate_games() {
+			try {
+				// сбрасываем рейтинг:
+				if (!$this->reset_rating())
+					return false;
+				// получаем идентификаторы всех сыгранных игр:
+				if (!($games_list = $this->get_games_list()))
+					return false;
+				for ($i=0; $i < count($games_list); $i++)
+				if (!$this->calculate_game($games_list[$i]['IDGame'])) {
+					$this->reset_rating();
+					return false;
+				}
+				return true;
+			} catch (Exception $e) {return $this->log_error($e->getMessage());}
+		}
+	}
+
+	/**
+	 * 2P рейтинг
+	 */
+	class DualDeploymentRating extends RWGBaseRating {
+		/**
+		 * Получить рейтинг игрока
+		 * @param {Number} $playerid id игрока
+		 * @return {Array} Массив вида array('Games'=>'кол-во сыгранных игр', 'Rating'=>'текущий рейтинг')
+		 */
+		public function get_player_rating($playerid) {
+			try {
+				// some code...
+			} catch (Exception $e) {return $this->log_error($e->getMessage());}
+		}
+		/**
+		 * Учёт игры в текущем рейтинге
+		 * @param {Number} $gameid id игры
+		 * @return {Boolean} Была ли учтена игра
+		 */
+		public function calculate_game($gameid) {
+			try {
+				// пытаемся получить стату по игре:
+				if (!($teams = $this->get_players_stats($gameid)))
+					return false;
+				// вытаскиваем настройки рейтинга:
+				$options = $this->options;
+				// проверяем соответствие количества игроков:
+				if (isset($options['MinPlayers'])) {
+					$players_num = 0;
+					foreach ($teams as $k=>$v) {
+						$players = $v['Players'];
+						foreach ($players as $l=>$b)
+						if (!$b['Watcher'])
+							$players_num++;
+					}
+					// если игроков слишком мало - не учитываем игру:
+					if ($options['MinPlayers'] > $players_num)
+						return true;
+				}
+				// проходимся по всем игрокам и считаем их "вклад" в игру:
+				foreach ($teams as $k=>$v) {
+					$team = $v['Players'];
+					$team_total = 0;
+					// считаем общее кол-во очков для текущей команды:
+					for ($j=0; $j < count($team); $j++) {
+						$player = $team[$j];
+						// пропускаем наблюдателей (в этой версии рейтинга они не получают бонусов за просмотр):
+						if ($player['Watcher']) continue;
+						// считаем очки для каждого игрока и команды:
+						$total = 0;
+						// если команда проиграла, считаем вклад в поражение, а иначе в победу:
+						if ($v['Win'] == 0) {
+							$total += $player['InfantryLoses']     * $options['Infantry'];
+							$total += $player['TanksLoses']        * $options['Tank'];
+							$total += $player['TrucksLoses']       * $options['Truck'];
+							$total += $player['AircraftLoses']     * $options['Aircraft'];
+							$total += $player['AntiAircraftLoses'] * $options['AntiAircraft'];
+							$total += $player['ArtilleryLoses']    * $options['Artillery'];
+							$total += $player['TrainsShipsLoses']  * $options['TrainsShip'];
+							$total += $player['UnknownLoses']      * $options['Unknown'];
+						} else {
+							$total += $player['InfantryKills']     * $options['Infantry'];
+							$total += $player['TanksKills']        * $options['Tank'];
+							$total += $player['TrucksKills']       * $options['Truck'];
+							$total += $player['AircraftKills']     * $options['Aircraft'];
+							$total += $player['AntiAircraftKills'] * $options['AntiAircraft'];
+							$total += $player['ArtilleryKills']    * $options['Artillery'];
+							$total += $player['TrainsShipsKills']  * $options['TrainsShip'];
+							$total += $player['UnknownKills']      * $options['Unknown'];
+						}
+						$team_total += $total;
+						// сохраняем кол-во очков для тек. игрока:
+						$teams[$k]['Players'][$j]['Total'] = $total;
+					}
+					// сохраняем кол-во очков для команды:
+					if ($v['Win'] == 0)
+						$teams[$k]['Total'] = -$team_total;
+					else
+						$teams[$k]['Total'] = $team_total;
+				}
+				
+				
+				
+				
+				
+				echo('<pre>');
+				print_r($teams);
+				echo('</pre>');
+				
+				echo('<br><br><pre>');
+				print_r($options);
+				echo('</pre>');
+				
+				return true;
 			} catch (Exception $e) {return $this->log_error($e->getMessage());}
 		}
 	}
